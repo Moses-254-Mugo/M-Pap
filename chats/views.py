2442051f312dabe89787254ef3def10b27c4ff22
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-11-03 21:04:08 +0300

    Add file

--- chats/views.py
+++ chats/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Room, Message
 # Create your views here.
 
 def home(request):
@@ -9,4 +9,12 @@ def room(request):
     return render(request, 'room.html')
 
 def checkview(request):
-    pass
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists(): # if the room name that was inputed exits
+        return redirect('/'+room+'/?username='+username) # Redirect to the room
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
