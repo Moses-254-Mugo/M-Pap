203c1dc93127fbff9ca9d4d22320f76ac751049e
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-11-04 16:12:19 +0300

    Add template registration

--- chats/views.py
+++ chats/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import redirect, render
 from .models import Room, Message
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'home.html')
 
